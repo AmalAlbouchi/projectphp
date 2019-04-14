b71184bfeef5ab0df01cbfa805e743710dd1f09d
Jihed Mastouri <25284659+Yaminokaze@users.noreply.github.com>
2019-04-14 01:35:32 +0100

    index

--- index.php
+++ index.php
@@ -3,9 +3,156 @@ $page=1;
 include ($_SERVER['DOCUMENT_ROOT'].'/projectphp/static/header.php');
 ?>
 
-<div>
-<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3192.2107213818936!2d10.186719814891982!3d36.86137467993596!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12e2cb4b0ecda8a3%3A0xc561ba5428c31de3!2sISI!5e0!3m2!1sen!2stn!4v1551527756480" width="100%" height="500" frameborder="0" style="border:0" allowfullscreen></iframe>
-</div>
+
+<section class="masthead">
+    <div class="container d-flex h-100 align-items-center">
+        <div class="mx-auto text-center">
+          <h1 class="mx-auto my-0 text-uppercase">PHOTOBOMB</h1>
+          <h2 class="text-white-50 mx-auto mt-2 mb-5">The Best Shop For PRO Photography</h2>
+          <a href="/pages/shop.php" class="btn btn-primary js-scroll-trigger">Visit Our Shop</a>
+        </div>
+      </div>
+</section>
+        
+
+<section id="projects" class="projects-section bg-light">
+    <div class="container">
+
+      <!-- Featured Project Row -->
+      <div class="row align-items-center no-gutters mb-4 mb-lg-5">
+        <div class="col-xl-8 col-lg-7">
+          <img class="img-fluid mb-3 mb-lg-0" src="/images/cam1.jpg" alt="">
+        </div>
+        <div class="col-xl-4 col-lg-5">
+          <div class="featured-text text-center text-lg-left">
+            <h4>DSLR</h4>
+            <p class="text-black-50 mb-0">
+                A digital single-lens reflex camera is a digital camera that combines the optics and the mechanisms of a single-lens reflex camera with a digital imaging sensor, as opposed to photographic film. The reflex design scheme is the primary difference between a DSLR and other digital cameras.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <!-- Project One Row -->
+      <div class="row justify-content-center no-gutters mb-5 mb-lg-0">
+        <div class="col-lg-6">
+          <img class="img-fluid" src="/images/cam2.jpg" alt="">
+        </div>
+        <div class="col-lg-6">
+          <div class="bg-black text-center h-100 project">
+            <div class="d-flex h-100">
+              <div class="project-text w-100 my-auto text-center text-lg-left">
+                <h4 class="text-white">Mirrorless</h4>
+                <p class="mb-0 text-white-50">
+                    A digital camera that accepts different lenses but does not use a mirror to reflect the image into the viewfinder. Also called a "mirrorless interchangeable-lens camera" (MILC)
+                </p>
+                <hr class="d-none d-lg-block mb-0 ml-0">
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- Project Two Row -->
+      <div class="row justify-content-center no-gutters">
+        <div class="col-lg-6">
+          <img class="img-fluid" src="/images/cam3.jpg" alt="">
+        </div>
+        <div class="col-lg-6 order-lg-first">
+          <div class="bg-black text-center h-100 project">
+            <div class="d-flex h-100">
+              <div class="project-text w-100 my-auto text-center text-lg-right">
+                <h4 class="text-white">Vintage</h4>
+                <p class="mb-0 text-white-50">
+                    A camera that exposes photographic film to light in order to take a picture. Used since the late 1800s, the film is a chemical emulsion on a plastic substrate that is sensitive to light.
+                </p>
+                <hr class="d-none d-lg-block mb-0 mr-0">
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="bg-light">
+    <div class="container">    
+      <div class="row">
+        <div class="col-md-4 col-lg-6" style="display:flex; align-items:center;">
+          <h3>Where You Can Find US : <br> <span class="section-subheading text-muted">Address: Rue Abourraihan Al Bayrouni, Ariana 2080 <br>
+              Phone: 71 706 164
+            </span> </h3>
+        </div>
+      <div class="col-md-4 col-lg-6">
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3192.2109636652317!2d10.186785114891979!3d36.86136887993601!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12e2cb4b0eb7c1d9%3A0x31dc6ea669baadcd!2sInstitut+sup%C3%A9rieur+d&#39;informatique%2C+Rue+Abourraihan+Al+Bayrouni%2C+Ariana!5e0!3m2!1sfr!2stn!4v1555200926604!5m2!1sfr!2stn" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+      </div>
+    </div>
+  </div>
+  </section>
+
+        <section class="bg-light" id="team">
+                <div class="container">
+                  <div class="row">
+                    <div class="col-lg-12 text-center">
+                      <h2 class="section-heading text-uppercase">Created By</h2>
+                      <h3 class="section-subheading text-muted">Institut Supérieur d'Informatique - 2eme SIL 2019 - with Love &hearts; </h3>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-sm-6">
+                      <div class="team-member">
+                        <img class="mx-auto rounded-circle" src="/images/jihed.jpg" alt="">
+                        <h4>Jihed Mastouri</h4>
+                        <p class="text-muted">Student</p>
+                        <ul class="list-inline social-buttons">
+                          <li class="list-inline-item">
+                            <a target="blank" href="https://twitter.com/MastouriJ">
+                              <i class="fab fa-twitter"></i>
+                            </a>
+                          </li>
+                          <li class="list-inline-item">
+                            <a target="blank" href="https://github.com/Yaminokaze">
+                                <i class="fab fa-github"></i>
+                            </a>
+                          </li>
+                          <li class="list-inline-item">
+                            <a target="blank" href="https://www.linkedin.com/in/jihedmastouri/">
+                              <i class="fab fa-linkedin-in"></i>
+                            </a>
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                    <div class="col-sm-6">
+                      <div class="team-member">
+                        <img class="mx-auto rounded-circle" src="/images/amal.jpg" alt="">
+                        <h4>Amal ALbouchi</h4>
+                        <p class="text-muted">Student</p>
+                        <ul class="list-inline social-buttons">
+                          <li class="list-inline-item">
+                            <a target="blank" href="mailto:amalalbouchie@yahoo.fr">
+                              <i class="fas fa-envelope-open-text"></i>
+                            </a>
+                          </li>
+                          <li class="list-inline-item">
+                            <a target="blank" href="https://github.com/AmalAlbouchi">
+                              <i class="fab fa-github"></i>
+                            </a>
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-lg-8 mx-auto text-center">
+                      <p class="large text-muted">
+                        This project was created from as PHP school project. we used Native PHP, MySQL, BootStrap. The project is an E-commerce web site model   that don't use CMS. 
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </section>
+
 
 
 <?php 
